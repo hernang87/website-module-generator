8696749076cd56bb3fc35c8d0b2805f09cb103d4
Hernan Garchtrom <hernan.garchtrom@rga.com>
2016-02-02 12:07:38 -0300

    Added support for modules with styles and scripts

--- Gulpfile.js
+++ Gulpfile.js
@@ -39,6 +39,22 @@ gulp.task('hbs', function () {
                     str += '<script src="' + js.url + '" type="text/javascript"></script>';
                 });
                 return new handlebars.Handlebars.SafeString(str);
+            },
+            modules: function(arr) {
+                var str = '';
+                arr.forEach(function(module) {
+                    str +=  str += '<link href="' + module.name + '" rel="stylesheet" type="text/css" />';
+                    str +=  str += '<link href="' + module.name + '.medium.css" rel="stylesheet" type="text/css" />';
+                    str +=  str += '<link href="' + module.name + '.large.css" rel="stylesheet" type="text/css" />';
+                });
+                return new handlebars.Handlebars.SafeString(str);
+            },
+            moduleScripts: function(arr) {
+                var str = '';
+                arr.forEach(function(js) {
+                    str += '<script src="js/' + js.name + '.js" type="text/javascript"></script>';
+                });
+                return new handlebars.Handlebars.SafeString(str);
             }
         }       
     };
